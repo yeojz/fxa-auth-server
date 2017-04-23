d9345cf151cac4ff97514bbcd23827d09ba02189
John Morrison <jrgmorrison@gmail.com>
2017-04-22 21:23:56 -0700

    fix(l10n): npm install l10n outside main node_modules

--- scripts/download_l10n.sh
+++ scripts/download_l10n.sh
@@ -13,4 +13,4 @@ DOWNLOAD_PATH="git://github.com/mozilla/fxa-content-server-l10n.git#$FXA_L10N_SH
 
 echo "Downloading L10N files from $DOWNLOAD_PATH..."
 # Download L10N using npm
-npm install $DOWNLOAD_PATH
+npm install --prefix assets $DOWNLOAD_PATH
